dd7945b119cc9411598b7630b21273f7697a82ff
afeezdev <afeezdev@gmail.com>
2021-08-17 18:35:47 +0100

    name displaying

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("ABDULYEKEEN AFEEZ");
+console.log("ABDULYEKEEN AFEEZ")
+
+let button = document.querySelector("button")
+
+button.addEventListener("click", function(){
+    document.getElementById("name").innerHTML = "ABDULYEKEEN AFEEZ";
+  });
